ab362f76cadd3d0d6f26725116fc8c016d5755b2
Adam <adamvibeo@gmail.com>
2026-03-02 14:38:36 +0000

    a

--- start-rslogic-client.bat
+++ start-rslogic-client.bat
@@ -9,9 +9,10 @@ set PYTHONUNBUFFERED=1
 set "RSLOGIC_ROOT=%ROOT%"
 set "PYTHONPATH=%RSLOGIC_ROOT%;%RSLOGIC_ROOT%rslogic\internal_tools\rstool-sdk\src"
 set "PY_EXE=%VENV%\Scripts\python.exe"
+set "LOG_FILE=%LOG_DIR%\\rslogic-client.log"
 if not exist "%PY_EXE%" (
   echo [ERROR] Python executable not found: %PY_EXE%
   echo [ERROR] Re-run installer.bat to recreate the environment.
   exit /b 1
 )
-"%PY_EXE%" -m rslogic.client.rsnode_client 1>>"%LOG_DIR%\rslogic-client-stdout.log" 2>>"%LOG_DIR%\rslogic-client-stderr.log"
+"%PY_EXE%" -u -m rslogic.client.rsnode_client 2>>"%LOG_FILE%" 1>>"%LOG_FILE%"
